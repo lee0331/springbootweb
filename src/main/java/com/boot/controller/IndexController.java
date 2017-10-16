0399b41a76a1c251af62928993372f7fbe1835c5
xingwuzhao <xingwuzhao@localhost>
2017-10-16 09:44:11 +0800

    暂时注释

--- src/main/java/com/boot/controller/IndexController.java
+++ src/main/java/com/boot/controller/IndexController.java
@@ -1,9 +1,9 @@
 package com.boot.controller;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.core.JmsTemplate;
+//import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -25,10 +25,10 @@ public class IndexController {
 	
 	@Autowired
 	private ConfigSettings configSettings;
-	@Autowired
-	private JmsTemplate jmsTemplate;
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
+//	@Autowired
+//	private JmsTemplate jmsTemplate;
+//	@Autowired
+//	private RabbitTemplate rabbitTemplate;
 	
 	@RequestMapping("/index")
 	public @ResponseBody String index() {
@@ -44,15 +44,15 @@ public class IndexController {
 		return new WiselyResponse("welcome to here! " + wiselyMessage.getName());
 	}
 	
-	@RequestMapping("/jmsSend")
-	public @ResponseBody String jmsSend() {
-		jmsTemplate.send("my-jms-destination", new MsgJMS());
-		return "sendJms";
-	}
-	
-	@RequestMapping("/rabbitSend")
-	public @ResponseBody String rabbitSend() {
-		rabbitTemplate.convertAndSend("rabbit-queue", "来自rabbitMq的问候");
-		return "sendRabbit";
-	}
+//	@RequestMapping("/jmsSend")
+//	public @ResponseBody String jmsSend() {
+//		jmsTemplate.send("my-jms-destination", new MsgJMS());
+//		return "sendJms";
+//	}
+//	
+//	@RequestMapping("/rabbitSend")
+//	public @ResponseBody String rabbitSend() {
+//		rabbitTemplate.convertAndSend("rabbit-queue", "来自rabbitMq的问候");
+//		return "sendRabbit";
+//	}
 }
--- src/main/java/com/boot/msg/MsgJMS.java
+++ src/main/java/com/boot/msg/MsgJMS.java
@@ -1,21 +1,22 @@
 package com.boot.msg;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-
-import org.springframework.jms.core.MessageCreator;
+//import javax.jms.JMSException;
+//import javax.jms.Message;
+//import javax.jms.Session;
+//
+//import org.springframework.jms.core.MessageCreator;
 
 /**
  * 使用JMS
  * @author xingwuzhao
  *
  */
-public class MsgJMS implements MessageCreator {
-
-	@Override
-	public Message createMessage(Session session) throws JMSException {
-		return session.createTextMessage("测试jms");
-	}
+public class MsgJMS 
+//	implements MessageCreator 
+{
+//	@Override
+//	public Message createMessage(Session session) throws JMSException {
+//		return session.createTextMessage("测试jms");
+//	}
 
 }
